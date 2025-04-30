3922585e571ec4c763b95d7432d7bdf81597a734
dayong <wangdayong228@163.com>
2025-04-30 10:39:21 +0800

    update

--- tunnle_proxy.sh
+++ tunnle_proxy.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -x
 # source .profile
-YIDAIYILU_SERVER=8.218.255.183
+YIDAIYILU_SERVER_ETH=8.218.255.183
 
 trap "trap - SIGTERM && kill 0" SIGINT SIGTERM EXIT
-autossh -M 0 -vvv -CN -p 22 -L 21000:127.0.0.1:21000 root@$YIDAIYILU_SERVER &
-autossh -M 0 -vvv -CN -p 22 -L 21001:127.0.0.1:21001 root@$YIDAIYILU_SERVER &
-autossh -M 0 -vvv -CN -p 22 -L 22002:127.0.0.1:22002 root@$YIDAIYILU_SERVER
+autossh -M 0 -vvv -CN -p 22 -L 21000:127.0.0.1:21000 root@$YIDAIYILU_SERVER_ETH &
+autossh -M 0 -vvv -CN -p 22 -L 21001:127.0.0.1:21001 root@$YIDAIYILU_SERVER_ETH &
+autossh -M 0 -vvv -CN -p 22 -L 22002:127.0.0.1:22002 root@$YIDAIYILU_SERVER_ETH &
+
+YIDAIYILU_SERVER_CFX=47.83.15.87
+
+trap "trap - SIGTERM && kill 0" SIGINT SIGTERM EXIT
+autossh -M 0 -vvv -CN -p 22 -L 22000:127.0.0.1:22000 root@$YIDAIYILU_SERVER_CFX &
+autossh -M 0 -vvv -CN -p 22 -L 22001:127.0.0.1:22001 root@$YIDAIYILU_SERVER_CFX &
+autossh -M 0 -vvv -CN -p 22 -L 22002:127.0.0.1:22002 root@$YIDAIYILU_SERVER_CFX &
+autossh -M 0 -vvv -CN -p 22 -L 22100:127.0.0.1:22100 root@$YIDAIYILU_SERVER_CFX &
+autossh -M 0 -vvv -CN -p 22 -L 22200:127.0.0.1:22200 root@$YIDAIYILU_SERVER_CFX &
+autossh -M 0 -vvv -CN -p 22 -L 22300:127.0.0.1:22300 root@$YIDAIYILU_SERVER_CFX
